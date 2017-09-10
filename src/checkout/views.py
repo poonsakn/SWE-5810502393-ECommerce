6b02608dbf0748f4a6093a64939dd2a1f68802ed
Poonsak Nuchsiri <poonsak.nuch@gmail.com>
2017-09-10 18:25:42 +0700

    @3:08:00

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -1,10 +1,25 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+import stripe
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
 
 @login_required
 def checkout(request):
-    context = {}
+    publishKey = settings.STRIPE_PUBLISHABLE_KEY
+    if request.method == 'POST':
+        token = request.POST('stripeToken')
+        charge = stripe.Charge.create(
+                amount=1000,
+                currency="usd",
+                description="Example charge",
+                source=token,
+        )
+    context = {'publishKey': publishKey}
     template = 'checkout.html'
     return render(request, template, context)
+
