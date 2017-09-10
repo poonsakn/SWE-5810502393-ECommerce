2369c7551cd0568aa4df73055142375a098097f8
Poonsak Nuchsiri <poonsak.nuch@gmail.com>
2017-09-10 14:30:22 +0700

    @2:16:15

--- src/contact/views.py
+++ src/contact/views.py
@@ -11,8 +11,7 @@ from .forms import contactForm
 def contact(request):
     title = 'Contact'
     form = contactForm(request.POST or None)
-    context = {'title': title, 'form': form, }
-
+    confirm_message = None
 
     if form.is_valid():
         name = form.cleaned_data['name']
@@ -24,6 +23,8 @@ def contact(request):
         send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
         title = "Thanks!"
         confirm_message = "Thanks, we will get right back to you."
-        context = {'title': title, 'confirm_message': confirm_message, }
+        form = None
+    
+    context = {'title': title, 'form': form, 'confirm_message': confirm_message, }
     template = 'contact.html'
     return render(request, template, context)
